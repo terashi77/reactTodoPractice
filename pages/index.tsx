48b275bacff149c7299c97d0d79307b0145c6cd8
kazuki terashima <komasu39perc_tera@yahoo.co.jp>
2020-06-04 15:03:40 +0900

    タスクの永続化

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,21 @@ import * as React from "react";
 import TodoList from "./../components/TodoList";
 import TodoForm from "./../components/TodoForm";
 import { TodoInterface } from "./../interfaces/TodoInterface";
+import taskStorage from "./../store/index";
 
 export default function Home() {
-  const [todos, setTodos] = React.useState<TodoInterface[]>([
-    { id: "xx", title: "yy", detail: "zz" },
-    { id: "oo", title: "pp", detail: "qq" },
-  ]);
+  const [todos, setTodos] = React.useState<TodoInterface[]>([]);
+
+  React.useEffect(() => {
+    console.log("fetch");
+    const fetchedTodos: TodoInterface[] = taskStorage.fetch();
+    setTodos(fetchedTodos);
+  }, []);
+
+  React.useEffect(() => {
+    console.log("update");
+    taskStorage.save(todos);
+  }, [todos]);
 
   function handleTodoCreate(todo: TodoInterface) {
     const newTodosState: TodoInterface[] = [...todos];
---
+++ store/TaskStorageInterface.ts
@@ -0,0 +1,6 @@
+import { TodoInterface } from "./../interfaces/TodoInterface";
+
+export interface TaskStrorageInterface {
+  fetch: () => TodoInterface[];
+  save: (tasks: TodoInterface[]) => void;
+}
